850d72d60304b662b4182d848f719bc79491fe2b
MutiaNandhika <mutianandhika@gmail.com>
2026-02-19 11:23:05 +0700

    kolom saw_score dan saw_rank

--- database/migrations/2026_02_19_041810_add_saw_score_and_rank_to_applications_table.php
+++ database/migrations/2026_02_19_041810_add_saw_score_and_rank_to_applications_table.php
@@ -1,15 +1,15 @@
+<?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     public function up()
     {
         Schema::table('applications', function (Blueprint $table) {
-            $table->float('saw_c1')->nullable();
-            $table->float('saw_c2')->nullable();
-            $table->float('saw_c3')->nullable();
-            $table->float('saw_score')->nullable();
+            $table->decimal('saw_score', 5, 3)->nullable();
             $table->integer('saw_rank')->nullable();
         });
     }
@@ -17,9 +17,7 @@ return new class extends Migration {
     public function down()
     {
         Schema::table('applications', function (Blueprint $table) {
-            $table->dropColumn([
-                'saw_c1','saw_c2','saw_c3','saw_score','saw_rank'
-            ]);
+            $table->dropColumn(['saw_score', 'saw_rank']);
         });
     }
 };
