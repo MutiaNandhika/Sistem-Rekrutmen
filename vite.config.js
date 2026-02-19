d575b6493fdf6333c2fdfbcd66d49c7ae7486c00
MutiaNandhika <mutianandhika@gmail.com>
2026-02-19 12:45:21 +0700

    vite

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,12 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/css/public/public.css',
+                'resources/css/public/dashboard.css',
+                'resources/css/auth/login.css',
+                'resources/css/auth/forgot-password.css',
+                'resources/js/profile/index.js',
                 'resources/js/app.js',
+                'resources/js/bootstrap.js',
             ],
             refresh: true,
         }),
