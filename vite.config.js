298edddd8bd32cfbdcf9c787ba7c32ada0339065
MutiaNandhika <mutianandhika@gmail.com>
2026-02-28 15:02:58 +0700

    fix: add forgot-password.css to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/css/auth/forgot-password.css',
             ],
             refresh: true,
         }),
